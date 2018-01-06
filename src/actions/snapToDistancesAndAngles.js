091efa45ab4710f25714e9e87fb7a00e043d106f
Alistair Jones <alistair.jones@gmail.com>
2018-01-06 18:48:48 +0000

    Horizontal snapping, including guides.

--- src/actions/snapToDistancesAndAngles.js
+++ src/actions/snapToDistancesAndAngles.js
@@ -1,23 +1,34 @@
 import {Point} from "../model/Point";
 
+const snapTolerance = 20
+
 const snapToDistancesAndAngles = (graph, excludedNodeId, naturalPosition) => {
   let x = naturalPosition.x, y = naturalPosition.y
-
-  let columns = [];
+  let columns = [], rows = [];
   graph.nodes.forEach((node) => {
     if (!node.idMatches(excludedNodeId)) {
       columns.push({
         x: node.position.x,
         distance: Math.abs(naturalPosition.x - node.position.x)
       })
+      rows.push({
+        y: node.position.y,
+        distance: Math.abs(naturalPosition.y - node.position.y)
+      })
     }
   })
-  columns.sort((a, b) => a.distance - b.distance)
+  const byDescendingDistance = (a, b) => a.distance - b.distance;
+  columns.sort(byDescendingDistance)
+  rows.sort(byDescendingDistance)
 
   let guidelines = []
-  if (columns[0] && columns[0].distance < 20) {
+  if (columns[0] && columns[0].distance < snapTolerance) {
     x = columns[0].x
     guidelines.push({type: 'VERTICAL', x})
+  }
+  if (rows[0] && rows[0].distance < snapTolerance) {
+    y = rows[0].y
+    guidelines.push({type: 'HORIZONTAL', y})
   }
 
   return {
