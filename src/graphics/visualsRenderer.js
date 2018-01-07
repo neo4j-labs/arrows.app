258e6b01d24ce16f14bd05eafcfd1c638afdc6bf
Irfan Nuri Karaca <irfannuri@gmail.com>
2018-01-07 16:08:25 +0000

    Extract rendering of visuals to a seperate module

---
+++ src/graphics/visualsRenderer.js
@@ -0,0 +1,19 @@
+import {drawGuideline, drawNode} from "../graphics/canvasRenderer";
+
+export const renderVisuals = ({visuals, canvas, displayOptions}) => {
+  const { graph, guides } = visuals
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, displayOptions.canvasSize.width, displayOptions.canvasSize.height);
+
+  guides.guidelines.forEach((guideline) => {
+    drawGuideline(ctx, guideline, displayOptions.canvasSize.width, displayOptions.canvasSize.height)
+  })
+
+  if (guides.naturalPosition) {
+    drawNode(ctx, displayOptions.viewTransformation.transform(guides.naturalPosition), 'grey', 50)
+  }
+
+  graph.nodes.forEach((node) => {
+    drawNode(ctx, displayOptions.viewTransformation.transform(node.position), '#53acf3', 50)
+  })
+}
