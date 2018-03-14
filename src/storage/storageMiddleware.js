35baa62f55f2557bc11ec40528bb4f295fab1dac
Alistair Jones <alistair.jones@gmail.com>
2018-03-14 14:57:17 +0000

    Move all nodes in a single session (=> single tx).

--- src/storage/storageMiddleware.js
+++ src/storage/storageMiddleware.js
@@ -5,11 +5,10 @@ const host = "bolt://localhost:7687"
 const driver = neo4j.driver(host, neo4j.auth.basic("neo4j", "a"))
 
 export const storageMiddleware = store => next => action => {
-  const runCypher = (cypher, parameters) => {
+  const runInSession = (work) => {
     store.dispatch(updatingGraph())
     const session = driver.session()
-    session
-      .run(cypher, parameters)
+    work(session)
       .then(() => {
         session.close()
         store.dispatch(updatingGraphSucceeded())
@@ -22,7 +21,7 @@ export const storageMiddleware = store => next => action => {
 
   switch (action.type) {
     case 'CREATE_NODE': {
-      runCypher('CREATE (:Diagram0 {_id: $id, _x: $x, _y: $y, _radius: $radius, ' +
+      runInSession((session) => session.run('CREATE (:Diagram0 {_id: $id, _x: $x, _y: $y, _radius: $radius, ' +
         '_caption: $caption, _color: $color})', {
         id: action.newNodeId,
         x: action.newNodePosition.x,
@@ -30,12 +29,12 @@ export const storageMiddleware = store => next => action => {
         radius: action.radius,
         caption: action.caption,
         color: action.color
-      })
+      }))
       break
     }
 
     case 'CREATE_NODE_AND_RELATIONSHIP': {
-      runCypher('MATCH (n:Diagram0 {_id: $sourceNodeId}) ' +
+      runInSession((session) => session.run('MATCH (n:Diagram0 {_id: $sourceNodeId}) ' +
         'CREATE (n)-[:_RELATED {_id: $newRelationshipId}]->' +
         '(:Diagram0 {_id: $targetNodeId, _x: $x, _y: $y, _radius: $radius, ' +
         '_caption: $caption, _color: $color})', {
@@ -47,26 +46,26 @@ export const storageMiddleware = store => next => action => {
         radius: action.radius,
         caption: action.caption,
         color: action.color
-      })
+      }))
       break
     }
 
     case 'CONNECT_NODES': {
-      runCypher('MATCH (source:Diagram0 {_id: $sourceNodeId}), (target:Diagram0 {_id: $targetNodeId}) ' +
+      runInSession((session) => session.run('MATCH (source:Diagram0 {_id: $sourceNodeId}), (target:Diagram0 {_id: $targetNodeId}) ' +
         'CREATE (source)-[:_RELATED {_id: $newRelationshipId}]->(target)', {
         sourceNodeId: action.sourceNodeId,
         newRelationshipId: action.newRelationshipId,
         targetNodeId: action.targetNodeId
-      })
+      }))
       break
     }
 
     case 'SET_NODE_CAPTION': {
-      runCypher('MATCH (n:Diagram0 {_id: $id}) ' +
+      runInSession((session) => session.run('MATCH (n:Diagram0 {_id: $id}) ' +
         'SET n._caption = $caption', {
         id: action.nodeId,
         caption: action.caption
-      })
+      }))
       break
     }
 
@@ -75,35 +74,39 @@ export const storageMiddleware = store => next => action => {
       action.keyValuePairs.forEach((keyValuePair) => {
         properties[keyValuePair.key] = keyValuePair.value
       })
-      runCypher('MATCH (n:Diagram0 {_id: $id}) ' +
+      runInSession((session) => session.run('MATCH (n:Diagram0 {_id: $id}) ' +
         'SET n += $properties', {
         id: action.nodeId,
         properties: properties
-      })
+      }))
       break
     }
 
     case 'MOVE_NODES': {
-      action.nodePositions.forEach((nodePosition) => {
-        runCypher('MATCH (n:Diagram0 {_id: $id}) ' +
-          'SET n._x = $x, n._y = $y', {
-          id: nodePosition.nodeId,
-          x: nodePosition.position.x,
-          y: nodePosition.position.y
+      runInSession((session) => {
+        let result = session
+        action.nodePositions.forEach((nodePosition) => {
+          result = session.run('MATCH (n:Diagram0 {_id: $id}) ' +
+            'SET n._x = $x, n._y = $y', {
+            id: nodePosition.nodeId,
+            x: nodePosition.position.x,
+            y: nodePosition.position.y
+          })
         })
+        return result
       })
       break
     }
 
     case 'SET_RELATIONSHIP_TYPE': {
-      runCypher(`MATCH (n)-[r]->(m)
+      runInSession((session) => session.run(`MATCH (n)-[r]->(m)
           WHERE r._id = $id
           CREATE (n)-[r2:${action.relationshipType}]->(m)
           SET r2 = r
           WITH r
           DELETE r`, {
         id: action.relationshipId
-      })
+      }))
       break
     }
 
