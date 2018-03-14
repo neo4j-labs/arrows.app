d346ab64e8840130621ce3504102e652ef64c9aa
Alistair Jones <alistair.jones@gmail.com>
2018-03-14 14:28:20 +0000

    Prove support for more than 10 nodes.

--- src/model/Id.js
+++ src/model/Id.js
@@ -8,7 +8,7 @@ export function idsMatch(a, b) {
 
 export function nextAvailableId(entities) {
   const currentIds = entities.map((entity) => entity.id)
-    .filter((id) => /n[0-9+]/.test(id))
+    .filter((id) => /^n[0-9]+$/.test(id))
     .map((id) => parseInt(id.substring(1)))
     .sort()
   return 'n' + (currentIds.length > 0 ? currentIds.pop() + 1 : 0)
--- src/model/Id.test.js
+++ src/model/Id.test.js
@@ -4,14 +4,23 @@ it('suggests n0 when there are no nodes supplied', () => {
   expect(id).toEqual('n0')
 })
 
-it('suggest n0 when all other ids are in a different format', () => {
+it('suggests n0 when all other ids are in a different format', () => {
   const id = nextAvailableId([
     { id: 'some_other_kind_of_id-0'}
   ])
   expect(id).toEqual('n0')
 })
 
-it('suggest n2 when n0 and n1 are taken', () => {
+it('suggests next available id when other ids are of mixed formats', () => {
+  const id = nextAvailableId([
+    { id: 'some_other_kind_of_id-0'},
+    { id: 'n1'},
+    { id: 'n2n'}
+  ])
+  expect(id).toEqual('n2')
+})
+
+it('suggests n2 when n0 and n1 are taken', () => {
   const id = nextAvailableId([
     { id: 'n0'},
     { id: 'n1'}
@@ -19,10 +28,24 @@ it('suggest n2 when n0 and n1 are taken', () => {
   expect(id).toEqual('n2')
 })
 
-it('suggest n2 when n1 is taken', () => {
+it('suggests n2 when n1 is taken', () => {
   const id = nextAvailableId([
     { id: 'n1'}
   ])
   expect(id).toEqual('n2')
+})
+
+it('suggests n10 when n9 is taken', () => {
+  const id = nextAvailableId([
+    { id: 'n9'}
+  ])
+  expect(id).toEqual('n10')
+})
+
+it('suggests n11 when n10 is taken', () => {
+  const id = nextAvailableId([
+    { id: 'n10'}
+  ])
+  expect(id).toEqual('n11')
 })
 
