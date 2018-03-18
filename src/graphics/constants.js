7e68dd5b7833e82d6964c7dac5dcd94f6f4a1147
Alistair Jones <alistair.jones@gmail.com>
2018-03-18 11:20:01 +0000

    More generous hit tolerance for relationships.

--- src/graphics/constants.js
+++ src/graphics/constants.js
@@ -3,4 +3,4 @@ export const defaultNewNodeRadius = 50;
 export const ringMargin = 10;
 export const arrowLength = 10
 export const arrowWidth = 7
-export const relationshipHitTolerance = 5
+export const relationshipHitTolerance = 20
