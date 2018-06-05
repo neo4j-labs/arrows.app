0105e1e39d96cb24db0d6b7344df18b0f8119e35
Alistair Jones <alistair.jones@gmail.com>
2018-06-05 18:19:36 +0100

    Use a constant snap tolerance.

--- src/actions/snapToTargetNode.js
+++ src/actions/snapToTargetNode.js
@@ -1,9 +1,11 @@
 import {closestNode} from "../model/Graph";
 import {idsMatch} from "../model/Id";
 
+const snapTolerance = 20
+
 const snapToTargetNode = (graph, excludedNodeId, naturalPosition) => {
   const targetNode = closestNode(graph, naturalPosition, (node, distance) => {
-    return !idsMatch(node.id, excludedNodeId) && distance < node.radius
+    return !idsMatch(node.id, excludedNodeId) && distance < snapTolerance
   })
 
   return {
