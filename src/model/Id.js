1a80b134883964cc9c513b9b84320d155e9b4891
Irfan Nuri Karaca <irfannuri@gmail.com>
2018-06-10 21:08:49 +0100

    Fix duplicate id generation issue

--- src/model/Id.js
+++ src/model/Id.js
@@ -10,6 +10,7 @@ export function nextAvailableId(entities) {
   const currentIds = entities.map((entity) => entity.id)
     .filter((id) => /^n[0-9]+$/.test(id))
     .map((id) => parseInt(id.substring(1)))
-    .sort()
+    .sort((x, y) => x-y)
+  
   return 'n' + (currentIds.length > 0 ? currentIds.pop() + 1 : 0)
 }
