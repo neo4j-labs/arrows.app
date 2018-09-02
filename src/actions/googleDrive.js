3acd2148eea9dd94ecde2612eac15ffb1b161c24
Alistair Jones <alistair.jones@gmail.com>
2018-09-02 10:59:44 +0100

    Create hello.json.

--- src/actions/googleDrive.js
+++ src/actions/googleDrive.js
@@ -1,6 +1,6 @@
 import config from "../config";
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
-const SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+const SCOPES = 'https://www.googleapis.com/auth/drive.file';
 
 export const saveGraphToGoogleDrive = () => {
 
@@ -8,6 +8,7 @@ export const saveGraphToGoogleDrive = () => {
     const isSignedIn = window.gapi.auth2.getAuthInstance().isSignedIn.get()
     console.log("signed in: ", isSignedIn)
     if (isSignedIn) listFiles()
+    if (isSignedIn) createFile({hello: true})
     return isSignedIn
   }
 
@@ -30,6 +31,39 @@ export const saveGraphToGoogleDrive = () => {
         appendPre('No files found.');
       }
     });
+  }
+
+  const createFile = (data) => {
+    const boundary = '-------314159265358979323846';
+    const delimiter = "\r\n--" + boundary + "\r\n";
+    const close_delim = "\r\n--" + boundary + "--";
+
+    const contentType = 'application/json';
+
+    const metadata = {
+      'name': "hello.json",
+      'mimeType': contentType
+    };
+
+    const multipartRequestBody =
+      delimiter +
+      'Content-Type: application/json\r\n\r\n' +
+      JSON.stringify(metadata) +
+      delimiter +
+      'Content-Type: ' + contentType + '\r\n\r\n' +
+      JSON.stringify(data) +
+      close_delim;
+
+    const request = window.gapi.client.request({
+      'path': '/upload/drive/v3/files',
+      'method': 'POST',
+      'params': {'uploadType': 'multipart'},
+      'headers': {
+        'Content-Type': 'multipart/related; boundary="' + boundary + '"'
+      },
+      'body': multipartRequestBody});
+
+    request.execute((file) => console.log(file));
   }
 
   window.gapi.client.init({
