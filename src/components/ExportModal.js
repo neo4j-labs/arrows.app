f9036845fbaa02f1285a1efcaacea38462d0478e
Alistair Jones <alistair.jones@gmail.com>
2018-11-09 15:30:14 +0000

    Use an off-screen canvas for rendering image.

--- src/components/ExportModal.js
+++ src/components/ExportModal.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Modal } from 'semantic-ui-react'
+import { Modal, Image } from 'semantic-ui-react'
 import {ViewTransformation} from "../state/ViewTransformation";
 import {getVisualGraph} from "../selectors/index";
 import {calculateBoundingBox} from "../graphics/utils/geometryUtils";
@@ -8,21 +8,34 @@ import {Vector} from "../model/Vector";
 
 class ExportModal extends Component {
 
-  constructor(props) {
-    super(props)
-    this.boundingBox = calculateBoundingBox(this.props.graph.nodes, defaultNewNodeRadius, 1) || {
-        left: 0, top: 0, right: 100, bottom: 100
-      }
-    this.boundingBox.width = this.boundingBox.right - this.boundingBox.left
-    this.boundingBox.height = this.boundingBox.bottom - this.boundingBox.top
-    this.scaleFactor = 2
-  }
-
   onCancel = () => {
     this.props.onCancel()
   }
 
   render() {
+    const boundingBox = calculateBoundingBox(this.props.graph.nodes, defaultNewNodeRadius, 1) || {
+        left: 0, top: 0, right: 100, bottom: 100
+      }
+    boundingBox.width = boundingBox.right - boundingBox.left
+    boundingBox.height = boundingBox.bottom - boundingBox.top
+    const scaleFactor = 2
+    const renderState = {
+      graph: this.props.graph,
+      selection: {
+        selectedNodeIdMap: {},
+        selectedRelationshipIdMap: {}
+      },
+      viewTransformation: new ViewTransformation(scaleFactor, new Vector(-scaleFactor * boundingBox.left, -scaleFactor * boundingBox.top))
+    }
+    const visualGraph = getVisualGraph(renderState)
+
+    const canvas = window.document.createElement('canvas')
+    canvas.width = Math.ceil(scaleFactor * boundingBox.width)
+    canvas.height = Math.ceil(scaleFactor * boundingBox.height)
+    const ctx = canvas.getContext('2d');
+    visualGraph.draw(ctx)
+    const dataUrl = canvas.toDataURL()
+
     return (
       <Modal
         size="large"
@@ -35,37 +48,11 @@ class ExportModal extends Component {
             maxHeight: 300,
             overflow: 'scroll'
           }}>
-          <canvas
-            width={Math.ceil(this.scaleFactor * this.boundingBox.width)}
-            height={Math.ceil(this.scaleFactor * this.boundingBox.height)}
-            ref={(canvas) => this.canvas = canvas}
-          />
+            <Image src={dataUrl}/>
           </div>
         </Modal.Content>
       </Modal>
     )
-  }
-
-  componentDidMount() {
-    this.draw()
-  }
-
-  componentDidUpdate() {
-    this.draw()
-  }
-
-  draw() {
-    const renderState = {
-      graph: this.props.graph,
-      selection: {
-        selectedNodeIdMap: {},
-        selectedRelationshipIdMap: {}
-      },
-      viewTransformation: new ViewTransformation(this.scaleFactor, new Vector(-this.scaleFactor * this.boundingBox.left, -this.scaleFactor * this.boundingBox.top))
-    }
-    const visualGraph = getVisualGraph(renderState)
-    const ctx = this.canvas.getContext('2d');
-    visualGraph.draw(ctx)
   }
 }
 
