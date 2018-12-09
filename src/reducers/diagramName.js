95ad601b4d55c8b410eab5217b45ee0967695cb4
Alistair Jones <alistair.jones@gmail.com>
2018-12-09 23:19:40 +0000

    Reset diagram name for new diagrams.

--- src/reducers/diagramName.js
+++ src/reducers/diagramName.js
@@ -1,5 +1,10 @@
-const diagramName = (state = 'Untitled graph', action) => {
+const defaultName = 'Untitled graph'
+
+const diagramName = (state = defaultName, action) => {
   switch (action.type) {
+    case 'NEW_DIAGRAM':
+      return defaultName
+
     case 'SET_DIAGRAM_NAME':
       return action.diagramName
 
