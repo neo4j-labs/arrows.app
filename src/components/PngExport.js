572838c5e44ed41595d84f92cdd85ae761cfbb12
Alistair Jones <alistair.jones@gmail.com>
2018-12-30 16:13:25 +0000

    Line breaks.

--- src/components/PngExport.js
+++ src/components/PngExport.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import { Image, Segment, Label, Icon } from 'semantic-ui-react'
+import {Image, Segment, Label, Icon} from 'semantic-ui-react'
 import {renderGraphAtScaleFactor} from "../graphics/utils/offScreenCanvasRenderer";
 
 class PngExport extends Component {
 
   render() {
-    const { width, height, dataUrl } = renderGraphAtScaleFactor(this.props.graph, this.props.pixelRatio, this.props.transparentBackground)
+    const {width, height, dataUrl} = renderGraphAtScaleFactor(this.props.graph, this.props.pixelRatio, this.props.transparentBackground)
 
     return (
       <Segment style={{
@@ -18,7 +18,10 @@ class PngExport extends Component {
         </Label>
         <div style={{
           display: 'inline-block',
-          backgroundImage: 'linear-gradient(45deg, #efefef 25%, transparent 25%), linear-gradient(-45deg, #efefef 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #efefef 75%), linear-gradient(-45deg, transparent 75%, #efefef 75%)',
+          backgroundImage: 'linear-gradient(45deg, #efefef 25%, transparent 25%), ' +
+          'linear-gradient(-45deg, #efefef 25%, transparent 25%), ' +
+          'linear-gradient(45deg, transparent 75%, #efefef 75%), ' +
+          'linear-gradient(-45deg, transparent 75%, #efefef 75%)',
           backgroundSize: '20px 20px',
           backgroundPosition: '0 0, 0 10px, 10px -10px, -10px 0px'
         }}>
