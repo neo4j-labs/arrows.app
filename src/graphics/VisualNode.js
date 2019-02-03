f7fd9a46f396e52d02f9a7595bfd8e3b7ced7584
Alistair Jones <alistair.jones@gmail.com>
2019-02-03 21:40:17 +0000

    Draw pills around the labels.

--- src/graphics/VisualNode.js
+++ src/graphics/VisualNode.js
@@ -126,10 +126,28 @@ export default class VisualNode {
     ctx.textBaseline = 'middle'
 
     ctx.translate(...position.translate(new Vector(radius, 0).rotate(Math.PI / 4)).xy)
+    const padding = 5
+    const margin = 4
+    const pillHeight = fontSize + padding * 2
+    const pillRadius = pillHeight / 2
+    const lineHeight = pillHeight + margin
+
     labels.forEach((label, i) => {
       ctx.save()
-      ctx.translate(0, i * fontSize)
-      drawTextLine(ctx, label, new Point(0, 0), false)
+      ctx.translate(-pillRadius, i * lineHeight - pillRadius)
+      const metrics = ctx.measureText(label)
+      ctx.beginPath()
+      ctx.moveTo(pillRadius, 0)
+      ctx.lineTo(pillRadius + metrics.width, 0)
+      ctx.arc(pillRadius + metrics.width, pillRadius, pillRadius, -Math.PI / 2, Math.PI / 2)
+      ctx.lineTo(pillRadius, pillHeight)
+      ctx.arc(pillRadius, pillRadius, pillRadius, Math.PI / 2, -Math.PI / 2)
+      ctx.closePath()
+      ctx.fillStyle = 'white'
+      ctx.fill()
+      ctx.stroke()
+      ctx.fillStyle = 'black'
+      drawTextLine(ctx, label, new Point(pillRadius, pillRadius), false)
       ctx.restore()
     })
     ctx.restore()
