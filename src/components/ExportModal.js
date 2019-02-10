f10d586b24e6d4edd98c881d4eeb8f535e353781
Alistair Jones <alistair.jones@gmail.com>
2019-02-10 09:23:33 +0000

    Extract PNG export into a smaller component.

--- src/components/ExportModal.js
+++ src/components/ExportModal.js
@@ -1,21 +1,8 @@
 import React, {Component} from 'react';
-import {Modal, Button, Message, Icon, Checkbox} from 'semantic-ui-react'
-import PngExport from "./PngExport";
+import {Modal, Button} from 'semantic-ui-react'
+import ExportPngPanel from "./ExportPngPanel";
 
 class ExportModal extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      transparentBackground: true
-    }
-  }
-
-  toggleTransparent = () => {
-    this.setState({
-      transparentBackground: !this.state.transparentBackground
-    })
-  }
 
   onCancel = () => {
     this.props.onCancel()
@@ -30,31 +17,10 @@ class ExportModal extends Component {
       >
         <Modal.Header>Export</Modal.Header>
         <Modal.Content scrolling>
-          <Message info icon>
-            <Icon name='info'/>
-            <p>
-              Below are a few PNGs at different "pixel ratios".
-              Choose your preferred resolution and then get hold of the image by:
-              <ul>
-                <li>Clicking on the "Download" link to download a file to your computer, or</li>
-                <li>Right-clicking on the image and choosing "Copy Image", or</li>
-                <li>Dragging the image to another browser tab or to another application.</li>
-              </ul>
-            </p>
-          </Message>
-          <Checkbox label='Transparent background' checked={this.state.transparentBackground} onChange={this.toggleTransparent}/>
-
-          {
-            [1, 2, 4].map((pixelRatio => (
-              <PngExport
-                graph={this.props.graph}
-                diagramName={this.props.diagramName}
-                pixelRatio={pixelRatio}
-                transparentBackground={this.state.transparentBackground}
-              />
-            )))
-          }
-
+          <ExportPngPanel
+            graph={this.props.graph}
+            diagramName={this.props.diagramName}
+          />
         </Modal.Content>
         <Modal.Actions>
           <Button
