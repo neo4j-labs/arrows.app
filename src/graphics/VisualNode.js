61788737c17651aa68a4d8a943acec4c70a686a4
Alistair Jones <alistair.jones@gmail.com>
2019-04-13 11:44:56 +0100

    Lots of styling for labels.

--- src/graphics/VisualNode.js
+++ src/graphics/VisualNode.js
@@ -116,18 +116,20 @@ export default class VisualNode {
 
   drawLabels(ctx, position, radius, labels) {
     ctx.save()
-    const fontSize = this['caption-font-size']
-    const fontColor = 'black' //this['caption-color']
+    const fontSize = this['label-font-size']
+    const fontColor = this['label-color']
+    const backgroundColor = this['label-background-color']
+    const strokeColor = this['label-border-color']
+    const strokeWidth = this['label-border-width']
     const fontFace = get(config, 'font.face')
+    const padding = this['label-padding']
+    const margin = this['label-margin']
 
-    ctx.fillStyle = fontColor
     let fontWeight = 'normal'
     ctx.font = `${fontWeight} ${fontSize}px ${fontFace}`
     ctx.textBaseline = 'middle'
 
     ctx.translate(...position.translate(new Vector(radius, 0).rotate(Math.PI / 4)).xy)
-    const padding = 5
-    const margin = 4
     const pillHeight = fontSize + padding * 2
     const pillRadius = pillHeight / 2
     const lineHeight = pillHeight + margin
@@ -143,10 +145,12 @@ export default class VisualNode {
       ctx.lineTo(pillRadius, pillHeight)
       ctx.arc(pillRadius, pillRadius, pillRadius, Math.PI / 2, -Math.PI / 2)
       ctx.closePath()
-      ctx.fillStyle = 'white'
+      ctx.fillStyle = backgroundColor
       ctx.fill()
+      ctx.strokeStyle = strokeColor
+      ctx.strokeWidth = strokeWidth
       ctx.stroke()
-      ctx.fillStyle = 'black'
+      ctx.fillStyle = fontColor
       drawTextLine(ctx, label, new Point(pillRadius, pillRadius), false)
       ctx.restore()
     })
--- src/selectors/style.js
+++ src/selectors/style.js
@@ -5,5 +5,11 @@ const graphStyleSelector = graph => graph.style || {}
 export const getStyleSelector = (entity, styleKey) =>
   createSelector(
     graphStyleSelector,
-    graphStyle => entity.style && entity.style[styleKey] || graphStyle[styleKey]
+    // graphStyle => entity.style && entity.style.hasOwnProperty(styleKey) || graphStyle[styleKey]
+    graphStyle => {
+      if (entity.style && entity.style.hasOwnProperty(styleKey)) {
+        return entity.style[styleKey]
+      }
+      return graphStyle[styleKey]
+    }
   )
