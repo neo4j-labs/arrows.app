b11c11b83b065ed30395953072e550f1352da9c9
Alistair Jones <alistair.jones@gmail.com>
2020-04-12 14:41:52 +0100

    Draw borders on the outside of nodes.
    This is noticeable with thicker borders, especially more than 5px.
    Drawing this way ensures that margins at preserved at the ends of
    arrows, and that bounding box calculations are correct.

--- src/graphics/NodeBackground.js
+++ src/graphics/NodeBackground.js
@@ -1,17 +1,19 @@
 export class NodeBackground {
-  constructor(style) {
+  constructor(position, internalRadius, style) {
+    this.position = position;
+    this.internalRadius = internalRadius;
     this.backgroundColor = style('node-color')
     this.borderWidth = style('border-width')
     this.borderColor = style('border-color')
   }
 
-  draw(position, radius, ctx) {
+  draw(ctx) {
     ctx.save()
 
     ctx.fillStyle = this.backgroundColor
     ctx.strokeStyle = this.borderColor
     ctx.lineWidth = this.borderWidth
-    ctx.circle(position.x, position.y, radius, true, this.borderWidth > 0)
+    ctx.circle(this.position.x, this.position.y, this.internalRadius + this.borderWidth / 2, true, this.borderWidth > 0)
     ctx.restore()
   }
 }
