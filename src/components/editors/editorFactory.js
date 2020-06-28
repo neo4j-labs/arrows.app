7f6221ab6f470aa4b9f5ce5b01204cfdc5a51b72
Alistair Jones <alistair.jones@gmail.com>
2020-06-28 15:49:47 +0100

    Push validation down to Slider.

--- src/components/editors/editorFactory.js
+++ src/components/editors/editorFactory.js
@@ -3,27 +3,23 @@ import {Input} from 'semantic-ui-react'
 import ColorPicker from './ColorPicker'
 import Slider from './Slider'
 import Dropdown from "./Dropdown";
-import {styleAttributes, styleTypes, validate} from "../../model/styling";
+import {styleAttributes, styleTypes} from "../../model/styling";
 
 export const getStyleEditorComponent = (styleKey, value, placeholder, onChange, onKeyPress, setFocusHandler) => {
   const attribute = styleAttributes[styleKey]
   const styleType = styleTypes[attribute.type]
-  const onChangeWithValidation = (value) => {
-    if (validate(styleKey, value) === value) {
-      onChange(value)
-    }
-  }
 
   switch (styleType.editor) {
     case 'slider':
       return (
         <Slider
+          styleKey={styleKey}
           value={value}
           min={styleType.min}
           max={styleType.max}
           step={styleType.step}
           placeholder={placeholder}
-          onChange={onChangeWithValidation}
+          onChange={onChange}
           onKeyPress={onKeyPress}
           setFocusHandler={setFocusHandler}
         />
@@ -33,7 +29,7 @@ export const getStyleEditorComponent = (styleKey, value, placeholder, onChange, 
         <ColorPicker
           value={value}
           placeholder={placeholder}
-          onChange={onChangeWithValidation}
+          onChange={onChange}
           onKeyPress={onKeyPress}
           setFocusHandler={setFocusHandler}
         />
@@ -44,7 +40,7 @@ export const getStyleEditorComponent = (styleKey, value, placeholder, onChange, 
           value={value}
           placeholder={placeholder}
           options={styleType.options}
-          onChange={onChangeWithValidation}
+          onChange={onChange}
         />
       )
     default:
@@ -53,7 +49,7 @@ export const getStyleEditorComponent = (styleKey, value, placeholder, onChange, 
           fluid
           value={value}
           placeholder={placeholder}
-          onChange={onChangeWithValidation}
+          onChange={onChange}
           onKeyPress={onKeyPress}
         />
       )
