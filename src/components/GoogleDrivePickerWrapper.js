7aa4434bf279cc2fd47c520f66d08cac85de9d2c
Irfan Nuri Karaca <irfannuri@gmail.com>
2020-07-05 17:10:41 +0300

    Fix the bug with google drive picker on first sign in

--- src/components/GoogleDrivePickerWrapper.js
+++ src/components/GoogleDrivePickerWrapper.js
@@ -6,7 +6,33 @@ export default class extends Component {
 
   componentDidMount () {
     this.createPicker()
-    this.picker.setVisible(true)
+  }
+
+  ensureGapiAndActWithToken(setupPicker) {
+    const showPicker = () => {
+      setupPicker(window.gapi.auth.getToken().access_token)
+      this.picker.setVisible(true)
+    }
+
+    if (window.gapi.auth2.getAuthInstance() && window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
+      showPicker()
+    } else {
+      window.gapi.client.init({
+        apiKey: config.apiKey,
+        clientId: config.clientId,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+      }).then(() => {
+        if (window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
+          showPicker()
+        } else {
+          window.gapi.auth2.getAuthInstance().signIn()
+            .then(() => {
+              showPicker()
+            })
+        }
+      })
+    }
   }
 
   createPicker() {
@@ -22,24 +48,7 @@ export default class extends Component {
         .build();
     }
 
-    if (window.gapi.auth2.getAuthInstance() && window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
-      const accessToken = window.gapi.auth.getToken().access_token
-      setupPicker(accessToken)
-    } else {
-      window.gapi.client.init({
-        apiKey: config.apiKey,
-        clientId: config.clientId,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-      }).then(() => {
-        if (window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
-          setupPicker(window.gapi.auth.getToken().access_token)
-        } else {
-          window.gapi.auth2.getAuthInstance().signIn();
-          setupPicker(window.gapi.auth.getToken().access_token)
-        }
-      })
-    }
+    this.ensureGapiAndActWithToken(setupPicker)
   }
 
   pickerCallback ({action, docs}) {
@@ -47,7 +56,6 @@ export default class extends Component {
       const fileId = docs[0].id
       this.props.onFilePicked(fileId)
     } else {
-      console.log("ACTION", action)
       this.props.onCancelPicker()
     }
   }
