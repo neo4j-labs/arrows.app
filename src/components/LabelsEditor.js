8ced37802ea5d314570c527d111987a1148e6ace
Alistair Jones <alistair.jones@gmail.com>
2020-07-07 18:59:37 +0100

    Remove edit-mode +Label and +Property buttons.
    They were poorly positioned and therefore distracting.
    Consider re-introducing when there is a better place to position them.

--- src/components/LabelsEditor.js
+++ src/components/LabelsEditor.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { Button } from 'semantic-ui-react'
 
 export class LabelsEditor extends Component {
 
@@ -13,51 +12,33 @@ export class LabelsEditor extends Component {
 
   render() {
     const nodeLabels = this.props.visualNode.labels
-    const { visualNode, selection, onAddLabel, onRenameLabel } = this.props
+    const {selection, onRenameLabel} = this.props
     const padding = 10
-    return (
-      <div>
-        {nodeLabels.pills.map((pill, index) => {
-          const position = nodeLabels.pillPositions[index]
-          const lineHeight = pill.font.fontSize * 1.2
-          return (
-            <input
-              key={'pill-' + index}
-              value={pill.text}
-              onChange={(e) => onRenameLabel(selection, pill.text, e.target.value)}
-              style={{
-                position: 'absolute',
-                padding: 0,
-                left: position.dx + pill.radius,
-                top: position.dy + pill.radius - lineHeight / 2,
-                width: pill.width + padding,
-                height: lineHeight,
-                resize: 'none',
-                outline: 'none',
-                border: 'none',
-                background: 'transparent',
-                textAlign: 'left',
-                ...pill.font,
-                lineHeight: 1.2
-              }}
-            />
-          )
-        })}
-        <Button
-          basic
-          key='addLabel'
-          onClick={() => onAddLabel(selection, '')}
-          size="tiny"
-          icon="plus"
-          content='Label'
-          type='button'
+    return nodeLabels.pills.map((pill, index) => {
+      const position = nodeLabels.pillPositions[index]
+      const lineHeight = pill.font.fontSize * 1.2
+      return (
+        <input
+          key={'pill-' + index}
+          value={pill.text}
+          onChange={(e) => onRenameLabel(selection, pill.text, e.target.value)}
           style={{
             position: 'absolute',
-            left: visualNode.position.x,
-            top: visualNode.position.y
+            padding: 0,
+            left: position.dx + pill.radius,
+            top: position.dy + pill.radius - lineHeight / 2,
+            width: pill.width + padding,
+            height: lineHeight,
+            resize: 'none',
+            outline: 'none',
+            border: 'none',
+            background: 'transparent',
+            textAlign: 'left',
+            ...pill.font,
+            lineHeight: 1.2
           }}
         />
-      </div>
-    )
+      )
+    })
   }
 }
