3d5b846660fdda14786ab8b4e59257acb76db386
Alistair Jones <alistair.jones@gmail.com>
2020-07-11 16:23:41 +0100

    Dark blue selection indicator when editing a node.

--- src/graphics/NodeBackground.js
+++ src/graphics/NodeBackground.js
@@ -1,9 +1,10 @@
-import {selectionBorder} from "../model/colors";
+import {selectionBorder, selectionHandle} from "../model/colors";
 
 export class NodeBackground {
-  constructor(position, internalRadius, style) {
+  constructor(position, internalRadius, editing, style) {
     this.position = position;
     this.internalRadius = internalRadius;
+    this.editing = editing
     this.backgroundColor = style('node-color')
     this.borderWidth = style('border-width')
     this.borderColor = style('border-color')
@@ -21,7 +22,7 @@ export class NodeBackground {
   drawSelectionIndicator(ctx) {
     ctx.save()
     const indicatorWidth = 10
-    ctx.strokeStyle = selectionBorder
+    ctx.strokeStyle = this.editing ? selectionHandle : selectionBorder
     ctx.lineWidth = indicatorWidth
     ctx.circle(this.position.x, this.position.y, this.internalRadius + this.borderWidth, false, true)
     ctx.restore()
--- src/graphics/Pill.js
+++ src/graphics/Pill.js
@@ -1,5 +1,5 @@
 import BoundingBox from "./utils/BoundingBox";
-import {selectionBorder} from "../model/colors";
+import {selectionBorder, selectionHandle} from "../model/colors";
 import {Point} from "../model/Point";
 
 export default class Pill {
@@ -45,7 +45,7 @@ export default class Pill {
   drawSelectionIndicator(ctx) {
     const indicatorWidth = 10
     ctx.save()
-    ctx.strokeStyle = selectionBorder
+    ctx.strokeStyle = this.editing ? selectionHandle : selectionBorder
     ctx.lineWidth = indicatorWidth
     ctx.lineJoin = 'round'
     ctx.rect(
