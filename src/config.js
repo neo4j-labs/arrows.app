5ef73078cd2b2e622f04caf62624b8e75ce1b287
Alistair Jones <alistair.jones@gmail.com>
2020-11-15 19:41:29 +0000

    New API Key.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  apiKey: "AIzaSyCmxlvbA9mEW5R6z23-planGaQoTnZu1oQ",
+  apiKey: "AIzaSyA-Xfd3gn0vlTdGjUGgW_bUnrzJxcq8_fo",
   discoveryDocs: ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.install'],
   clientId: "792493152966-jfbhng6ok6bd3ml7ott7ncflf5s49eas.apps.googleusercontent.com",
   appId: "792493152966"
