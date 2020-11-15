f48fdfcdeee3101f502ab1465bc99343b5ea9e1f
Alistair Jones <alistair.jones@gmail.com>
2020-11-15 19:32:28 +0000

    New OAuth Client ID.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   apiKey: "AIzaSyCmxlvbA9mEW5R6z23-planGaQoTnZu1oQ",
   discoveryDocs: ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.install'],
-  clientId: "342108448020-nhatc4lcbj81jrr1lhk925ahqfs6s98t.apps.googleusercontent.com",
-  appId: "342108448020"
+  clientId: "792493152966-jfbhng6ok6bd3ml7ott7ncflf5s49eas.apps.googleusercontent.com",
+  appId: "792493152966"
 };
