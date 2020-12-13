39b0fa3b1dc9113cbedd26b86e2c33195daabe1c
Alistair Jones <alistair.jones@gmail.com>
2020-12-13 10:48:45 +0000

    Replace help GIFs with YouTube video.

--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -1,17 +1,10 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux"
-import {Modal, Segment, Image, Button, Message} from 'semantic-ui-react'
-import help_add_node from  '../help/help_add_node.gif'
-import help_drag_to_create from  '../help/help_drag_to_create.gif'
-import help_drag_to_connect from  '../help/help_drag_to_connect.gif'
-import help_select from  '../help/help_select.gif'
-import help_caption from  '../help/help_caption.gif'
-import help_add_property from  '../help/help_add_property.gif'
-import help_duplicate from  '../help/help_duplicate.gif'
-import help_scale from  '../help/help_scale.gif'
+import {Button, Modal} from 'semantic-ui-react'
 import {hideHelpDialog} from "../actions/applicationDialogs";
 import {rememberHelpDismissed} from "../actions/localStorage";
 import {informationLinks} from "./informationLinks";
+import YouTube from "react-youtube";
 
 class HelpModal extends Component {
 
@@ -32,65 +25,18 @@ class HelpModal extends Component {
         </p>
       )
     })
-
     return (
       <Modal
-        size="fullscreen"
+        size="small"
         open={this.props.showModal}
         onClose={this.onCancel}
       >
         <Modal.Header>Help</Modal.Header>
         <Modal.Content scrolling>
-          <div
-            style={{
-              padding: '2px 10px',
-              display: 'flex',
-              flexDirection: 'row',
-            }}>
-            {links}
-          </div>
-          <Message
-            icon='help'
-            header='New to Arrows?'
-            content="These looping GIFs might help you to work out what you can do."
-          />
-          <div style={{display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-around'}}>
-          <Segment compact>
-            <h3>Create a node</h3>
-            <Image src={help_add_node}/>
-          </Segment>
-          <Segment compact>
-            <h3>Drag to Create Nodes and Relationships</h3>
-            <Image src={help_drag_to_create}/>
-          </Segment>
-          <Segment compact>
-            <h3>Drag to Connect Nodes</h3>
-            <Image src={help_drag_to_connect}/>
-          </Segment>
-          <Segment compact>
-            <h3>Selection</h3>
-            <Message>Tip: hold down the command key (Mac) to select multiple nodes.</Message>
-            <Image src={help_select}/>
-          </Segment>
-          <Segment compact>
-            <h3>Choose a Caption</h3>
-            <Image src={help_caption}/>
-          </Segment>
-          <Segment compact>
-            <h3>Add Properties</h3>
-            <Image src={help_add_property}/>
-          </Segment>
-          <Segment compact>
-            <h3>Duplicate Nodes and Relationships</h3>
-            <Message>Use ⌘D to duplicate nodes. This makes it easy to quickly create a large graph.</Message>
-            <Image src={help_duplicate}/>
-          </Segment>
-          <Segment compact>
-            <h3>Spread-out or flip the layout</h3>
-            <Message>Tip: Use ⌘A to select the whole graph.</Message>
-            <Image src={help_scale}/>
-          </Segment>
-          </div>
+          <YouTube videoId="ZHJ-BrKJ8A4" opts={{
+            height: '360',
+            width: '640',
+          }} />
         </Modal.Content>
         <Modal.Actions>
           <Button
