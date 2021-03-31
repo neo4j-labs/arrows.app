011b1078f66513202d49ae7026ef9672a4927674
Alistair Jones <alistair.jones@gmail.com>
2021-03-31 23:01:35 +0100

    Place imported graphs on the right.

--- src/actions/import.js
+++ src/actions/import.js
@@ -2,12 +2,20 @@ import {importNodesAndRelationships} from "./graph";
 import {Point} from "../model/Point";
 import {getPresentGraph, getVisualGraph} from "../selectors";
 import {constructGraphFromFile} from "../storage/googleDriveStorage";
+import {translate} from "../model/Node";
+import {Vector} from "../model/Vector";
+import BoundingBox from "../graphics/utils/BoundingBox";
 
 export const handlePaste = (pasteEvent) => {
   return function (dispatch, getState) {
     const state = getState()
     const graph = getPresentGraph(state)
     const visualGraph = getVisualGraph(state)
+    const boundingBox = visualGraph.boundingBox() || new BoundingBox(0,0,0,0)
+    const vector = new Vector(
+      boundingBox.right + graph.style.radius * 1.5,
+      boundingBox.top + graph.style.radius
+    )
 
     const clipboardData = pasteEvent.clipboardData
     const textPlainMimeType = 'text/plain'
@@ -16,7 +24,8 @@ export const handlePaste = (pasteEvent) => {
       const format = formats.find(format => format.recognise(text))
       if (format) {
         try {
-          const importedGraph = format.parse(text, graph, visualGraph)
+          const importedGraph = format.parse(text, graph)
+          importedGraph.nodes = importedGraph.nodes.map(node => translate(node, vector))
           dispatch(importNodesAndRelationships(importedGraph))
         } catch (e) {
           console.error(e)
@@ -30,26 +39,29 @@ const formats = [
   {
     // JSON
     recognise: (plainText) => new RegExp('^{.*\}$', 's').test(plainText.trim()),
-    parse: (plainText, graph, visualGraph) => {
+    parse: (plainText) => {
       const object = JSON.parse(plainText)
       const graphData = constructGraphFromFile(object)
-      return graphData.graph
+      const { nodes, relationships } = graphData.graph
+      const left = Math.min(...nodes.map(node => node.position.x))
+      const top = Math.min(...nodes.map(node => node.position.y))
+      const vector = new Vector(-left, -top)
+      const originNodes = nodes.map(node => translate(node, vector))
+      return {
+        nodes: originNodes, relationships
+      }
     }
   },
   {
     // plain text
     recognise: () => true,
-    parse: (plainText, graph, visualGraph) => {
+    parse: (plainText, graph) => {
       const lines = plainText.split('\n').filter(line => line && line.trim().length > 0)
-
-      const boundingBox = visualGraph.boundingBox()
-      const top = boundingBox.top + graph.style.radius
-      const left = boundingBox.right + graph.style.radius
 
       const nodes = lines.map((line, i) => {
         return {
           id: 'n' + i,
-          position: new Point(left, top + 3 * graph.style.radius * i),
+          position: new Point(0, 2.5 * graph.style.radius * i),
           caption: line,
           style: {},
           labels: [],
