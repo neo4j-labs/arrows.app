f12692eab548b8bfdd573bd625519a23842b3d15
Alistair Jones <alistair.jones@gmail.com>
2021-04-01 19:50:38 +0100

    Consolidate logic to import.js

--- src/actions/import.js
+++ src/actions/import.js
@@ -4,12 +4,37 @@ import {getPresentGraph} from "../selectors";
 import {constructGraphFromFile} from "../storage/googleDriveStorage";
 import {translate} from "../model/Node";
 import {Vector} from "../model/Vector";
+import {hideImportDialog} from "./applicationDialogs";
+
+export const tryImport = (dispatch) => {
+  return function (text, separation) {
+    let importedGraph
+
+    const format = formats.find(format => format.recognise(text))
+    if (format) {
+      try {
+        importedGraph = format.parse(text, separation)
+      } catch (e) {
+        return {
+          errorMessage: e.toString()
+        }
+      }
+    } else {
+      return {
+        errorMessage: 'No format found'
+      }
+    }
+
+    dispatch(importNodesAndRelationships(importedGraph))
+    dispatch(hideImportDialog())
+    return {}
+  }
+}
 
 export const handlePaste = (pasteEvent) => {
   return function (dispatch, getState) {
     const state = getState()
-    const graph = getPresentGraph(state)
-    const separation = graph.style.radius * 2.5
+    const separation = nodeSeparation(state)
 
     const clipboardData = pasteEvent.clipboardData
     const textPlainMimeType = 'text/plain'
@@ -28,7 +53,7 @@ export const handlePaste = (pasteEvent) => {
   }
 }
 
-export const formats = [
+const formats = [
   {
     // JSON
     recognise: (plainText) => new RegExp('^{.*\}$', 's').test(plainText.trim()),
@@ -68,3 +93,8 @@ export const formats = [
     }
   }
 ]
+
+export const nodeSeparation = (state) => {
+  const graph = getPresentGraph(state)
+  return graph.style.radius * 2.5
+}
--- src/containers/ImportContainer.js
+++ src/containers/ImportContainer.js
@@ -1,44 +1,18 @@
 import {connect} from "react-redux"
 import ImportModal from "../components/ImportModal";
 import {hideImportDialog} from "../actions/applicationDialogs";
-import {importNodesAndRelationships} from "../actions/graph";
-import {getPresentGraph} from "../selectors";
-import {formats} from "../actions/import";
+import {nodeSeparation, tryImport} from "../actions/import";
 
 const mapStateToProps = (state) => {
-  const graph = getPresentGraph(state)
-  const separation = graph.style.radius * 2.5
-
   return {
-    separation
+    separation: nodeSeparation(state)
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    tryImport: (text, separation) => {
-      let importedGraph
-
-      const format = formats.find(format => format.recognise(text))
-      if (format) {
-        try {
-          importedGraph = format.parse(text, separation)
-        } catch (e) {
-          return {
-            errorMessage: e.toString()
-          }
-        }
-      } else {
-        return {
-          errorMessage: 'No format found'
-        }
-      }
-
-      dispatch(importNodesAndRelationships(importedGraph))
-      dispatch(hideImportDialog())
-      return {}
-    },
-    onCancel: () =>{
+    tryImport: tryImport(dispatch),
+    onCancel: () => {
       dispatch(hideImportDialog())
     }
   }
