e703cd02644070b7c6ce9e9e35ba5144e03b96b3
Alistair Jones <alistair.jones@gmail.com>
2021-04-05 10:44:25 +0100

    Only show "Inline as properties" when appropriate.
    Added new conditions:
    * No nodes selected
    * All target nodes have to have at least one property
      (otherwise we're just deleting stuff)

--- src/components/DetailToolbox.js
+++ src/components/DetailToolbox.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Button, Form} from 'semantic-ui-react'
-import {selectedRelationshipIds, selectedRelationships} from "../model/selection";
+import {selectedNodeIds, selectedRelationshipIds, selectedRelationships} from "../model/selection";
 
 export const DetailToolbox = (props) => {
   const relationshipToolboxItems = (
@@ -37,18 +37,24 @@ export const DetailToolbox = (props) => {
   )
 
   const someRelationshipsSelected = selectedRelationshipIds(props.selection).length > 0
-  const selectedRelationshipsDoNotFormPaths = disjointSourceAndTargetSets(props.graph, props.selection)
+  const showInlineRelationshipsButton = shouldShowInlineRelationshipsButton(props.graph, props.selection)
 
   return (
     <Form.Field>
       {someRelationshipsSelected ? relationshipToolboxItems : null}
-      {someRelationshipsSelected && selectedRelationshipsDoNotFormPaths ? inlineRelationshipButton : null}
+      {showInlineRelationshipsButton ? inlineRelationshipButton : null}
       {selectionToolboxItems}
     </Form.Field>
   )
 }
 
-const disjointSourceAndTargetSets = (graph, selection) => {
+const shouldShowInlineRelationshipsButton = (graph, selection) => {
+  // only relationships selected
+  if (selectedRelationshipIds(selection).length < 1 || selectedNodeIds(selection).length > 0) {
+    return false
+  }
+
+  // disjoint source and target node sets
   const sourceNodeIds = new Set()
   const targetNodeIds = new Set()
   for (const relationship of selectedRelationships(graph, selection)) {
@@ -56,5 +62,17 @@ const disjointSourceAndTargetSets = (graph, selection) => {
     targetNodeIds.add(relationship.toId)
   }
   const intersection = new Set([...sourceNodeIds].filter(x => targetNodeIds.has(x)))
-  return intersection.size === 0
+  if (intersection.size > 0) {
+    return false
+  }
+
+  // all target nodes have properties
+  for (const targetNodeId of targetNodeIds) {
+    const targetNode = graph.nodes.find(node => node.id === targetNodeId)
+    if (Object.entries(targetNode.properties).length === 0) {
+      return false
+    }
+  }
+
+  return true
 }
