1f37cba76437649db9d00e27e6ead7218cfc8de2
Oskar Hane <oh@oskarhane.com>
2021-08-03 16:11:47 +0200

    Format file contents

--- src/graphql/relNameToInterfaceName.js
+++ src/graphql/relNameToInterfaceName.js
@@ -1,4 +1,9 @@
-export default function relNameToPropertyName(relName = '') {
-    const prefixedLowerCase = '_' + relName.toLowerCase().replace(/[^a-z0-9_]/g, '');
-    return prefixedLowerCase.split('_').filter(part => !!part).map(part => part[0].toUpperCase() + part.slice(1)).join('')
+export default function relNameToPropertyName(relName = "") {
+  const prefixedLowerCase =
+    "_" + relName.toLowerCase().replace(/[^a-z0-9_]/g, "");
+  return prefixedLowerCase
+    .split("_")
+    .filter((part) => !!part)
+    .map((part) => part[0].toUpperCase() + part.slice(1))
+    .join("");
 }
