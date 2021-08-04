616591415f9051491e15df6640477d129511dd3a
Oskar Hane <oh@oskarhane.com>
2021-08-04 08:37:28 +0200

    Formatting

--- src/graphql/relNameToInterfaceName.test.js
+++ src/graphql/relNameToInterfaceName.test.js
@@ -1,17 +1,16 @@
-import relNameToInterfaceName from "./relNameToInterfaceName"
+import relNameToInterfaceName from "./relNameToInterfaceName";
 
-describe('relNameToInterfaceName utility function', () => {
+describe("relNameToInterfaceName utility function", () => {
   const cases = [
-    ['input', 'Input'],
-    ['INPUT', 'Input'],
-    ['input_string', 'InputString'],
-    ['INPUT_STRING', 'InputString'],
-    ['INPUT____STRING', 'InputString'],
-    ['_INPUT____STRING', 'InputString'],
-    ['::::::a', 'A'],
-  ]
-  test.each(cases)(
-    `formats %s to %s`, 
-    (input, output) => expect(relNameToInterfaceName(input)).toEqual(output)
-  )
-})
+    ["input", "Input"],
+    ["INPUT", "Input"],
+    ["input_string", "InputString"],
+    ["INPUT_STRING", "InputString"],
+    ["INPUT____STRING", "InputString"],
+    ["_INPUT____STRING", "InputString"],
+    ["::::::a", "A"],
+  ];
+  test.each(cases)(`formats %s to %s`, (input, output) =>
+    expect(relNameToInterfaceName(input)).toEqual(output)
+  );
+});
