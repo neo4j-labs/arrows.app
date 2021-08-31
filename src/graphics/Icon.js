6473115a69e71006fd299b6fd3f639e33cdb9ef9
jharris4 <harris.jb@gmail.com>
2021-08-31 10:42:06 +0100

    fix for icon size when image width or height is 0

--- src/graphics/Icon.js
+++ src/graphics/Icon.js
@@ -3,9 +3,13 @@ export class Icon {
     this.iconImage = style(imageKey)
     const iconSize = style('icon-size')
     this.imageInfo = imageCache[this.iconImage]
-    const largestDimension = this.imageInfo.width > this.imageInfo.height ? 'width' : 'height'
-    this.width = largestDimension === 'width' ? iconSize : iconSize * this.imageInfo.width / this.imageInfo.height
-    this.height = largestDimension === 'height' ? iconSize : iconSize * this.imageInfo.height / this.imageInfo.width
+    if (this.imageInfo.width === 0 || this.imageInfo.height === 0) {
+      this.width = this.height = iconSize
+    } else {
+      const largestDimension = this.imageInfo.width > this.imageInfo.height ? 'width' : 'height'
+      this.width = largestDimension === 'width' ? iconSize : iconSize * this.imageInfo.width / this.imageInfo.height
+      this.height = largestDimension === 'height' ? iconSize : iconSize * this.imageInfo.height / this.imageInfo.width
+    }
   }
 
   draw(ctx, x, y) {
