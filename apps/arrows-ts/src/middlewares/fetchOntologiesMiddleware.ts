70a47db3cd51c59d59a535ccb108416dbb9c188b
Simone Guzzi <simone.guzzi@sib.swiss>
2024-07-14 19:49:00 +0200

    feat(arrows-ts): load all ontologies

--- apps/arrows-ts/src/middlewares/fetchOntologiesMiddleware.ts
+++ apps/arrows-ts/src/middlewares/fetchOntologiesMiddleware.ts
@@ -3,18 +3,22 @@ import {
   loadOntologiesSuccess,
 } from '../actions/ontologies';
 import { Action, Dispatch } from 'redux';
-import { ontologies } from '@neo4j-arrows/ontology-search';
+import { ontologies, ontologiesCount } from '@neo4j-arrows/ontology-search';
 
 export const fetchOntologiesMiddleware =
   (store) => (next: Dispatch) => (action: Action) => {
     const result = next(action);
 
     if (action.type === 'GETTING_GRAPH') {
-      ontologies()
-        .then((ontologies) => store.dispatch(loadOntologiesSuccess(ontologies)))
-        .catch((error) => {
-          store.dispatch(loadOntologiesFailure());
-        });
+      ontologiesCount().then((count) => {
+        ontologies(count)
+          .then((ontologies) =>
+            store.dispatch(loadOntologiesSuccess(ontologies))
+          )
+          .catch((error) => {
+            store.dispatch(loadOntologiesFailure());
+          });
+      });
     }
 
     return result;
