dc2b00e4a098214907031aa1021c5560a21e503c
Simone Guzzi <simone.guzzi@sib.swiss>
2024-08-13 17:00:17 +0200

    feat(linkml): add toGraph function

--- libs/linkml/src/index.ts
+++ libs/linkml/src/index.ts
@@ -1,4 +1,11 @@
-import { Graph } from '@neo4j-arrows/model';
+import {
+  Graph,
+  RelationshipType,
+  Node,
+  Relationship,
+  Cardinality,
+  Ontology,
+} from '@neo4j-arrows/model';
 import { LinkMLClass, LinkML, SpiresCoreClasses } from './lib/types';
 import {
   findNodeFactory,
@@ -13,6 +20,13 @@ import {
 } from './lib/relationships';
 import { nodeToClass } from './lib/nodes';
 import { toPrefixes } from './lib/ontologies';
+
+type LinkMLNode = Omit<Node, 'style' | 'position'>;
+type LinkMLRelationship = Omit<Relationship, 'style'>;
+type LinkMLGraph = {
+  nodes: LinkMLNode[];
+  relationships: LinkMLRelationship[];
+};
 
 export const fromGraph = (
   name: string,
@@ -83,5 +97,67 @@ export const fromGraph = (
         {}
       ),
     },
+  };
+};
+
+export const toGraph = (
+  { classes }: LinkML,
+  ontologies: Ontology[]
+): LinkMLGraph => {
+  const nodes: LinkMLNode[] = Object.entries(classes)
+    .filter(([key, value]) => value.is_a === SpiresCoreClasses.NamedEntity)
+    .map(([key, { attributes, id_prefixes }], index) => {
+      return {
+        id: index.toString(),
+        caption: key,
+        properties: Object.entries(attributes ?? {}).reduce(
+          (properties: Record<string, string>, [key, { description }]) => ({
+            ...properties,
+            [key]: description ?? '',
+          }),
+          {}
+        ),
+        entityType: 'node',
+        ontologies:
+          id_prefixes &&
+          ontologies.filter((ontology) =>
+            id_prefixes.includes(ontology.id.toLocaleUpperCase())
+          ),
+      };
+    });
+  const relationships: LinkMLRelationship[] = Object.entries(classes)
+    .filter(([key, value]) => value.is_a === SpiresCoreClasses.Triple)
+    .map(([key, { slot_usage }], index) => {
+      if (!slot_usage) {
+        return null;
+      }
+      const fromNode = nodes.find(
+        (node) => node.caption === slot_usage['subject'].range
+      );
+      const toNode = nodes.find(
+        (node) => node.caption === slot_usage['object'].range
+      );
+
+      if (!fromNode || !toNode) {
+        return null;
+      }
+      return {
+        relationshipType: RelationshipType.ASSOCIATION,
+        fromId: fromNode.id,
+        toId: toNode.id,
+        properties: {},
+        entityType: 'relationship',
+        type: '',
+        id: index.toString(),
+        cardinality: Cardinality.ONE_TO_MANY,
+      };
+    })
+    .filter(
+      (relationship: LinkMLRelationship | null) => !!relationship
+      // Casting as we are filtering out nulls anyway.
+    ) as LinkMLRelationship[];
+  return {
+    nodes,
+    relationships,
   };
 };
