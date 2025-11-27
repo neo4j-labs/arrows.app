9a4dc0b0d68c0b8d94d69a629b21fe2148780bd4
Rafał Janicki <7746477+rafaljanicki@users.noreply.github.com>
2025-11-27 13:05:38 +0100

    Move apiKey constant to an environment variable

--- apps/arrows-app/src/config.js
+++ apps/arrows-app/src/config.js
@@ -1,5 +1,5 @@
 export default {
-  apiKey: "AIzaSyA-Xfd3gn0vlTdGjUGgW_bUnrzJxcq8_fo",
+  apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
   discoveryDocs: ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.install'],
   clientId: "792493152966-jfbhng6ok6bd3ml7ott7ncflf5s49eas.apps.googleusercontent.com",
   appId: "792493152966"
