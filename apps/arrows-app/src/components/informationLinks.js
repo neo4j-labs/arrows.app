23c0006bd045458e9d18320f8cd16f5d03ef46ca
Andreas Kollegger <akollegger@gmail.com>
2026-02-23 13:52:03 +0000

    footer-update: updated privacy notice

--- apps/arrows-app/src/components/informationLinks.js
+++ apps/arrows-app/src/components/informationLinks.js
@@ -1,5 +1,5 @@
 export const informationLinks = [
-  ['Privacy', 'https://neo4j.com/privacy-policy/'],
+  ['Privacy', 'https://neo4j.com/legal-terms/privacy-notice/'],
   ['Terms', 'https://neo4j.com/terms'],
   ['Feedback', 'https://github.com/neo4j-labs/arrows.app'],
-]
+];
